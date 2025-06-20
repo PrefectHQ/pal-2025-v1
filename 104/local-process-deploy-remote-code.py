607e94ac99e360bf28d0ac423ea544e141ecc458
Bianca <105657837+biancaines@users.noreply.github.com>
2025-06-20 19:39:14 -0400

    Revert "PAL cleanup"

--- 104/local-process-deploy-remote-code.py
+++ 104/local-process-deploy-remote-code.py
@@ -8,9 +8,9 @@ def my_flow(name: str = "World"):
 
 if __name__ == "__main__":
     my_flow.from_source(
-        source="https://github.com/PrefectHQ/pal-2025-v1.git",  # code stored in GitHub
+        source="https://github.com/PrefectHQ/pacc-2025-v1.git",  # code stored in GitHub
         entrypoint="104/local-process-deploy-remote-code.py:my_flow",
     ).deploy(
-        name="pal-local-process-deploy-remote-code",
-        work_pool_name="pal-process-pool",
+        name="pacc-local-process-deploy-remote-code",
+        work_pool_name="pacc-process-pool",
     )
--- 105/sell.py
+++ 105/sell.py
@@ -10,7 +10,7 @@ def sell(ticker: str = "AAPL"):
 
 if __name__ == "__main__":
     sell.from_source(
-        source="https://github.com/prefecthq/pal-2025-v1.git",
+        source="https://github.com/prefecthq/pacc-2025-v1.git",
         entrypoint="105/sell.py:sell",
     ).deploy(
         name="selling-deployment",
