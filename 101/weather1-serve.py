561e498696ae287330ba4947da9673f55d219988
zeeshan <zeeshan.nu@outlook.com>
2025-07-28 17:38:44 +0500

    Add Prefect Basics module and enhance weather serve script documentation

--- 101/weather1-serve.py
+++ 101/weather1-serve.py
@@ -15,4 +15,18 @@ def fetch_weather(lat: float = 38.9, lon: float = -77.0):
 
 
 if __name__ == "__main__":
-    fetch_weather.serve(name="deploy-1")
+    """
+    Additional serve options
+    The serve method on flows exposes many options for the deployment. Here’s how to use some of those options:
+    https://docs.prefect.io/v3/how-to-guides/deployment_infra/run-flows-in-local-processes#serve-a-flow
+    The serve method on flows exposes many options for the deployment. Here’s how to use some of those options:
+    cron: a keyword that allows you to set a cron string schedule for the deployment; see schedules for more advanced scheduling options
+    tags: a keyword that allows you to tag this deployment and its runs for bookkeeping and filtering purposes
+    description: a keyword that allows you to document what this deployment does; by default the description is set from the docstring of the flow function (if documented)
+    version: a keyword that allows you to track changes to your deployment; uses a hash of the file containing the flow by default; popular options include semver tags or git commit hashes
+    triggers: a keyword that allows you to define a set of conditions for when the deployment should run; see triggers for more on Prefect Events concepts
+    """
+
+    get_weather = fetch_weather
+    get_weather.serve(name="deploy-3",
+                        tags=["production", "weather", "api","3"], )
